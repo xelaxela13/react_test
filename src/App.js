9648a30a49096212db52ba1e3c5e26598043efe2
xela <xelaxela13@gmail.com>
2020-01-04 12:55:02 +0200

    add packages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+      return <h1>Hello</h1>
+  }
+}
+
+export default App;
