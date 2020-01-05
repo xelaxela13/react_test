74ec99e25f2142b1fdbc393ca5913fea8b0b9c1f
xela <xelaxela13@gmail.com>
2020-01-05 13:11:56 +0200

    monthlist

--- src/App.js
+++ src/App.js
@@ -1,73 +1,35 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Monthlist from "./components/celendar/Monthlist";
-import Get from "./actions/fetch";
+import Fetch from 'react-fetch-component'
 
 const url = 'https://yalantis-react-school.herokuapp.com/api/task0/users';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
-        };
-        this.parseMonth = this.parseMonth.bind(this)
-    }
-
-    componentDidMount() {
-        let response = Get(url);
-        console.log(response)
-        if (response) {
-            this.setState({
-                isLoaded: true,
-                items: response['result']
-            });
-        }
-        // fetch(url)
-        //     .then(res => res.json())
-        //     .then(
-        //         (result) => {
-        //             this.setState({
-        //                 isLoaded: true,
-        //                 items: result
-        //             });
-        //         },
-        //         (error) => {
-        //             this.setState({
-        //                 isLoaded: true,
-        //                 error
-        //             });
-        //         }
-        //     )
-    }
-
-    parseMonth(dateString) {
-        let dateObject = new Date(dateString);
-        return dateObject.getMonth() + 1
-    }
-
     render() {
-        const {error, isLoaded, items} = this.state;
-        if (error) {
-            return <div>Ошибка: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Загрузка...</div>;
-        } else {
-            return (
-                <div>
-                    <Monthlist/>
-                    <ul>
-                        {items.map(item => (
-                            <li key={item.id}>
-                                {item.id} {item.firstName} {item.lastName} {item.dob} {this.parseMonth(item.dob)}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            );
-        }
+        return (
+            <Fetch url={url}>
+                {({loading, error, data}) => (
+                    <div>
+                        {loading && <h1>{loading}</h1>}
+                        {error && <h1>{error}</h1>}
+                        {data &&
+                        <div>
+                            <Monthlist items={data}/>
+                            <ul>
+                                {data.map(item => (
+                                    <li key={item.id}>
+                                        {item.id} {item.firstName} {item.lastName} {item.dob}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        }
+                    </div>
+                )}
+            </Fetch>
+
+        );
     }
 }
 
--- src/components/celendar/Monthlist.js
+++ src/components/celendar/Monthlist.js
@@ -7,27 +7,55 @@ class Monthlist extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            months: []
+            months: [],
+            itemsPMonth: {'1':[],'2':[],'3':[],'4':[],'5':[],'6':[],'7':[],'8':[],'9':[],'10':[],'11':[],'12':[]},
+            colors: {2: 'gray', 6: 'blue', 10: 'green', 11: 'red'}
         };
-        // this.moveEvent = this.moveEvent.bind(this)
+        this.parseMonth = this.parseMonth.bind(this);
+        this.getColor = this.getColor.bind(this)
     }
 
     componentDidMount() {
         this.setState({
-            months: monthNames
+            months: monthNames,
+            itemsPMonth: this.props.items.map(item=>{this.state.itemsPMonth[this.parseMonth(item.dob)].push(item)})
         });
     }
+
 
     moveEvent({event, start, end}) {
 
         console.log(event)
     }
+    getColor(itemLength){
+        let color;
+        if(itemLength < 3){
+            color = 'gray'
+        }else if(2 < itemLength < 7){
+            color = 'blue'
+        }else if(6 < itemLength < 11){
+            color = 'green'
+        }else if(itemLength > 10){
+            color = 'red'
+        }
+        return color
+    }
+
+    parseMonth(dateString) {
+        let dateObject = new Date(dateString);
+        return dateObject.getMonth() + 1
+    }
 
     render() {
-        const {months} = this.state;
+        const {months, itemsPMonth} = this.state;
+        console.log(itemsPMonth)
         return (
             <div className="d-flex justify-content-around my-3">
-                {months.map((element, index)=>(<button key={index} className="btn btn-primary">{element}</button>))}
+                {months.map((element, index) => (
+                    <button key={index} style={{backgroundColor: this.getColor(itemsPMonth[index+1]) }} className="btn btn-primary">
+                        {element}
+                    </button>
+                ))}
             </div>
         );
     }
